4a1985f953e9e9a18fc533c3501b26fe7de61645
antidfs <a5816097@aoyama.jp>
2018-07-29 23:34:25 +0900

    webapp to submit(2)

--- mytimeconv/views.py
+++ mytimeconv/views.py
@@ -2,15 +2,6 @@ from django.shortcuts import render
 from datetime import datetime
 from pytz import timezone
 
-def addDateStr(tz_name: str, tz_str: str, date_dict = {}):  #入力したタイムゾーンをフォーマットしたものの文字列を作成
-    try:
-        date = datetime.now(timezone(tz_str))
-        date_str = "{0:%Y-%m-%d %H:%M:%S}".format(date)
-
-        date_dict[tz_name] = date_str;
-    except:
-        print("タイムゾーン名 \"" + tz_str + "\" の追加に失敗しました(addDateStr)")
-
 def makeDateStr(tz_dict = {}, date_dict = {}):  #入力したタイムゾーンをフォーマットしたものの文字列を作成
     while tz_dict:
         tz_name, tz_str = tz_dict.popitem()
@@ -36,18 +27,5 @@ def appmain(request):
 
     makeDateStr(tz_name_dict, date_dict)
 
-    """
-    date_utc = datetime.now(timezone('UTC'))
-    date_utc_str = "{0:%Y-%m-%d %H:%M:%S}".format(date_utc)
-
-    date_jst = datetime.now(timezone('Asia/Tokyo'))
-    date_jst_str = "{0:%Y-%m-%d %H:%M:%S}".format(date_jst)
-
-    date_new_york = datetime.now(timezone('America/New_York'))
-    date_new_york_str = "{0:%Y-%m-%d %H:%M:%S}".format(date_new_york)
-
-    date_test = datetime.now(timezone('America/Detroit'))
-    date_test_str = "{0:%Y-%m-%d %H:%M:%S}".format(date_test)
-"""
     return render(request, "mytimeconv/mytimeconv_page.html", date_dict)
 # Create your views here.
